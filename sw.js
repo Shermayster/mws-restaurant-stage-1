499715f33961582936937845d09dd56f75d4f555
pavelshermayster <sherm84@gmail.com>
2018-03-19 15:34:35 +0200

    feat: added basic service worker functionality + accessibility fixes

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
         // create cache
-        caches.open('static') 
-            .then((cache) => {
-                cache.addAll([
-                    './',
-                    './index.html',
-                    './js/main.js',
-                    './js/dbhelper.js',
-                    './js/restaurant_info.js',
-                    './css/styles.css',
-                    './css/styles-medium.css',
-                ]);
-            })); 
+        caches.open('static')
+        .then((cache) => {
+            cache.addAll([
+                './',
+                './index.html',
+                './js/main.js',
+                './js/dbhelper.js',
+                './js/restaurant_info.js',
+                './css/styles.css',
+                './css/styles-medium.css',
+            ]);
+        }));
 });
 
 self.addEventListener('activate', (event) => {
@@ -21,9 +21,18 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request) 
-            .then((response) => {
-                return response ? response :
-                    fetch(event.request);
-            }));
+        caches.match(event.request)
+        .then((response) => {
+            // try to load from cache
+            return response ? response :
+                // add response to dynamic cache
+                fetch(event.request).then((res) => {
+                    caches.open('dynamic').then((cache) => {
+                        cache.put(event.request.url, res.clone());
+                        // return response to the app
+                        return res;
+                    });
+                })
+                .catch(err => console.log(err));
+        }));
 });
